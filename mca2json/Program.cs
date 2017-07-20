09b3986aaa39904010a25a29c6018590d2d014aa
yuletian <469632477@qq.com>
2017-07-20 23:12:00 +0800

    fc

---
+++ mca2json/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mca2json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((32>>5) & 31);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ mca2json/Region.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mca2json
+{
+    struct Region
+    {
+        public int regionX;
+        public int regionY;
+
+    }
+}
